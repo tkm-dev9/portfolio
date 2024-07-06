8ce46237deda81056ef0b3a77027112bceb76315
takumu1011 <sumikawa.web@gmail.com>
2024-07-07 01:18:29 +0900

    edit settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,11 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/portfolio/" : "./",
-  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     include: ["./src/tests/**/*.test.tsx"],
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
   },
+  plugins: [react(), tsconfigPaths()],
 });
