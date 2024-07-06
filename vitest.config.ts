21a02e233bf2b6d28c6cc38568969f5fc8deba6f
takumu1011 <sumikawa.web@gmail.com>
2024-07-07 01:01:51 +0900

    edit vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "portfolio" : "./",
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
