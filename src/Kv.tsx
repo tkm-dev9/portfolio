06310f7ca38d2ad859223448d22119746bfd7cff
takumu1011 <sumikawa.web@gmail.com>
2024-07-07 19:13:08 +0900

    adjust desing

--- src/Kv.tsx
+++ src/Kv.tsx
@@ -1,7 +1,7 @@
 export default function Kv() {
   const base = import.meta.env.BASE_URL;
   return (
-    <div className="mb-10">
+    <div className="flex flex-col justify-center h-screen mb-10 bg-blue-50">
       <img
         src={`${base}cat.png`}
         alt=""
