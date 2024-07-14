618c8b34854b04797d6a8eab1e65e56bb97b6dd0
takumu1011 <sumikawa.web@gmail.com>
2024-07-14 21:12:10 +0900

    adjust link

--- src/LinksArea.tsx
+++ src/LinksArea.tsx
@@ -30,18 +30,6 @@ export default function LinksArea() {
             className="absolute top-1/2 right-2 -translate-y-1/2 w-8"
           />
         </a>
-        <a
-          href="https://zenn.dev/tkm99"
-          className="relative flex items-center gap-5 w-60 p-4 bg-slate-300 cursor-pointer transition-opacity duration-700 ease-out hover:opacity-60"
-        >
-          <img src={`${base}zenn-logo.svg`} alt="" className="w-14" />
-          <span className="text-2xl">Zenn</span>
-          <img
-            src={`${base}external_link_icon.png`}
-            alt=""
-            className="absolute top-1/2 right-2 -translate-y-1/2 w-8"
-          />
-        </a>
       </div>
     </section>
   );
